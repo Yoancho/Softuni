86955504412d1c816a8ac37726590bdcd732d5eb
Yoancho <yoanivanov@mail.bg>
2019-01-03 22:18:28 +0200

    Update

--- JavaScript Core Fundamentals/Exam - 1 Sept 2016/2. Bunny Kill/solve.js
+++ JavaScript Core Fundamentals/Exam - 1 Sept 2016/2. Bunny Kill/solve.js
@@ -1,10 +1,43 @@
-function f(matrix) {
+function f(dataRows) {
+    let matrix = [];
+
+    for (var i = 0; i < dataRows.length - 1; i++) {
+        matrix.push(dataRows[i].split(' ').map(Number));
+    }
+    let coordinates = dataRows[dataRows.length - 1].split(' ');
+    let bunnyDamage = 0;
+    let bunnyKils = 0;
+    for (let token of coordinates) {
+        let [row, col] = token.split(',').map(Number);
+        if (matrix[row][col] > 0){
+            bunnyDamage += matrix[row][col];
+            bunnyKils++;
+            explode(row, col, matrix);
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] > 0){
+                bunnyDamage += matrix[i][j];
+                bunnyKils++;
+            }
+        }
+    }
+
+    console.log(bunnyDamage);
+    console.log(bunnyKils);
 
 
     function explode(row, col, matrix) {
+        // let startRow = Math.max(0, row - 1);
+        // let endRow = Math.min(matrix.length, row + 1);
+        // let startCol = Math.max(0, row - 1);
+
         let bunnyDamage = matrix[row][col];
-        for (var i = 0; i < col.length; i++) {
-            for (var j = 0; j < col.length; j++) {
+
+        for (let i = row - 1; i <= row + 1; i++) {
+            for (let j = col - 1; j <= col + 1; j++) {
                 if (isInside(i, j, matrix)) {
                     matrix[i][j] -= bunnyDamage;
                 }
@@ -13,16 +46,24 @@ function f(matrix) {
     }
 
     function isInside(row, col, matrix) {
-        return row >= 0
-            && row < matrix.length
-            && col >= 0
-            && col < matrix[row].length;
+        return row >= 0 && row < matrix.length && col >= 0 && col < matrix[row].length;
     }
 
-
-    // let startRow = Math.max(0, row - 1);
-    // let endRow = Math.min(matrix.length, row + 1);
-    // let startCol = Math.max(0, col - 1);
-    // let endCol = Math.min(matrix[row].length, col + 1)
-
 }
+
+
+f([
+    '10 10 10',
+    '10 10 10',
+    '10 10 10',
+    '0,0']);
+
+console.log();
+
+f([
+    '5 10 15 20',
+    '10 10 10 10',
+    '10 15 10 10',
+    '10 10 10 10',
+    '2,2 0,1'
+]);
