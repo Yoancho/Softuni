b9cb85f58b268a92cb9e3f2b04d9595825c387a5
Yoancho <yoanivanov@mail.bg>
2019-01-06 11:34:13 +0200

    Update solve

--- JavaScript Core Fundamentals/Exam - 11 Feb 2018/1. Bitcoin Mining/solve.js
+++ JavaScript Core Fundamentals/Exam - 11 Feb 2018/1. Bitcoin Mining/solve.js
@@ -1,38 +1,43 @@
 function f(arr) {
-    let leva = 0;
-    let bitcoin = 0;
-    let dayOfFirst = 0;
-
+    let bitcoins = 0;
+    let money = 0;
+    let dayOfTheFirstPurchasedBitcoin = 0;
+    const bitcoinPrice = 11949.16;
+    const goldPrice = 67.51;
+    arr.map(Number);
     for (let i = 1; i <= arr.length; i++) {
-
-        let mined = (Number(arr[i - 1]) * 67.51).toFixed(2);
+        let dailyGold = arr[i - 1];
         if (i % 3 === 0) {
-            mined = mined * 0.70;
+            dailyGold = dailyGold * 0.7;
         }
-        leva += +mined;
-        if (leva >= 11949.16) {
-            while (leva >= 11949.16){
-                bitcoin++;
-                leva = leva - 11949.16;
+        money += dailyGold * goldPrice;
+        if (money >= bitcoinPrice) {
+            if (dayOfTheFirstPurchasedBitcoin === 0) {
+                dayOfTheFirstPurchasedBitcoin = i;
             }
-            if (dayOfFirst === 0) {
-                dayOfFirst = i;
+            while (money >= bitcoinPrice) {
+                bitcoins++;
+                money -= bitcoinPrice;
             }
         }
     }
-    console.log(`Bought bitcoins: ${bitcoin}`);
-    if (dayOfFirst !== 0) {
-        console.log(`Day of the first purchased bitcoin: ${dayOfFirst}`);
+    console.log(`Bought bitcoins: ${bitcoins}`);
+    if (dayOfTheFirstPurchasedBitcoin !== 0) {
+        console.log(`Day of the first purchased bitcoin: ${dayOfTheFirstPurchasedBitcoin}`);
     }
 
-    console.log(`Left money: ${leva.toFixed(2)} lv.`);
+    console.log(`Left money: ${money.toFixed(2)} lv.`);
 }
 
 
-f(['0.05', '-200', '500']);
-// f(['100', '200', '300']);
-//
-// console.log();
-// f([50, 100]);
-// console.log();
-// f([3124.15, 504.212, 2511.124]);
+
+
+f([100, 200, 300]);
+
+console.log();
+
+f([50, 100]);
+
+console.log();
+
+f([3124.15, 504.212, 2511.124]);
